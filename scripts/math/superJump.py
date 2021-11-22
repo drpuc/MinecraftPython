3a19bdf506782d51828e414baf0f92e65c8742b2
drpuc <s_k_y_fox@list.ru>
2021-11-22 13:45:06 +0300

    Удалена папка

--- scripts/math/superJump.py
+++
@@ -1,9 +0,0 @@
-from mcpi.minecraft import Minecraft
-mc = Minecraft.create()
-position = mc.player.getTilePos()
-x = position.x
-y = position.y
-z = position.z
-
-y = y + 100
-mc.player.setTilePos(x, y, z)
